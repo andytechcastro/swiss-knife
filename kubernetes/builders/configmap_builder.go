d0bf1791d3697e7d8dd656fad7607c46479973af
Andrés Castro <andy.tech.castro@gmail.com>
2023-08-30 17:50:34 +0200

    Configmap

---
+++ kubernetes/builders/configmap_builder.go
@@ -0,0 +1,91 @@
+package builders
+
+import (
+	"fmt"
+
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/yaml"
+)
+
+// ConfigMap struct for configmap kubernetes resource
+type ConfigMap struct {
+	Name        string
+	Namespace   string
+	Labels      map[string]string
+	Annotations map[string]string
+	Data        map[string]string
+	Immutable   bool
+	ConfigMap   *apiv1.ConfigMap
+}
+
+// NewConfigMapBuilder return a ConfigMap
+// Immutable is false for default
+func NewConfigMapBuilder() *ConfigMap {
+	return &ConfigMap{
+		Immutable: false,
+	}
+}
+
+// SetName set the configmap name
+func (c *ConfigMap) SetName(name string) *ConfigMap {
+	c.Name = name
+	return c
+}
+
+// SetNamespace set the configmap namespace
+func (c *ConfigMap) SetNamespace(namespace string) *ConfigMap {
+	c.Namespace = namespace
+	return c
+}
+
+// SetLabels set labels in the configmap
+func (c *ConfigMap) SetLabels(labels map[string]string) *ConfigMap {
+	c.Labels = labels
+	return c
+}
+
+// SetData set the data of the configmap
+func (c *ConfigMap) SetData(data map[string]string) *ConfigMap {
+	c.Data = data
+	return c
+}
+
+// AddData add data to data configmap
+func (c *ConfigMap) AddData(key string, value string) *ConfigMap {
+	c.Data[key] = value
+	return c
+}
+
+// SetImmutable set the configmap immutable
+func (c *ConfigMap) SetImmutable() *ConfigMap {
+	c.Immutable = true
+	return c
+}
+
+// Build build a configmap
+func (c *ConfigMap) Build() *apiv1.ConfigMap {
+	configMap := &apiv1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      c.Name,
+			Namespace: c.Namespace,
+		},
+		Data:      c.Data,
+		Immutable: &c.Immutable,
+	}
+	c.ConfigMap = configMap
+	return configMap
+}
+
+// ToYaml convert deployment struct to kubernetes yaml
+func (c *ConfigMap) ToYaml() []byte {
+	yaml, err := yaml.Marshal(c.ConfigMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return yaml
+}
---
+++ kubernetes/builders/confimap_builder_test.go
@@ -0,0 +1,51 @@
+package builders_test
+
+import (
+	"testing"
+
+	"github.com/andytechcastro/swiss-knife/kubernetes/builders"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+)
+
+func initConfigMap() *builders.ConfigMap {
+	configmap := builders.NewConfigMapBuilder()
+	configmap.SetName("configmap").
+		SetNamespace("default").
+		SetData(map[string]string{
+			"myKey": "myValue",
+		})
+	return configmap
+}
+
+func TestBuildConfigMap(t *testing.T) {
+	configmap := initConfigMap()
+	buildedConfigMap := configmap.Build()
+	assert.Equal(t, "configmap", buildedConfigMap.Name)
+}
+
+func TestConfigMapToYaml(t *testing.T) {
+	configmap := initConfigMap()
+	configmap.SetImmutable()
+	configmap.AddData("secondKey", "secondValue")
+	configmap.Build()
+	yamlCM := configmap.ToYaml()
+	interfaceResult := map[string]interface{}(
+		map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "ConfigMap",
+			"metadata": map[string]interface{}{
+				"creationTimestamp": interface{}(nil),
+				"name":              "configmap",
+				"namespace":         "default",
+			},
+			"data": map[string]interface{}{
+				"myKey":     "myValue",
+				"secondKey": "secondValue",
+			},
+			"immutable": true,
+		},
+	)
+	yamlResult, _ := yaml.Marshal(interfaceResult)
+	assert.YAMLEq(t, string(yamlCM), string(yamlResult))
+}
